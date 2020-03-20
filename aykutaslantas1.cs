6383badeec4bd26aa22d48ed73743bdcc9c8f7f2
Aykut İsmet Aslantaş <aykur111aykur@gmail.com>
2020-03-20 22:17:47 +0300

    2 Sayıyı Toplayan C#  Uygulaması

---
+++ aykutaslantas1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aykutaslantas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2, toplam;
+            Console.Write("1. Sayıyı Giriniz :");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. Sayyıyı Giriniz :");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            toplam = sayi1 + sayi2;
+            Console.Write("Sayıların Toplamı ={0}",toplam);
+            Console.ReadKey();//aykutaslantass
+        }
+    }
+}
